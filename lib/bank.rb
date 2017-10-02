8e8a858317f51123171d7291801b596ac99eae83
Jenny Bacon <jenniferbacon01@gmail.com>
2017-10-02 12:45:59 +0100

    ordering of transactions

--- lib/bank.rb
+++ lib/bank.rb
@@ -18,17 +18,18 @@ class Bank
   end
 
   def print_statement
-    statement_str = 'date || credit || debit || balance'
+    statement_str = ''
     historic_balance = 0
     @transactions.each do |transaction|
       if transaction.credit_amount > 0
         historic_balance += transaction.credit_amount
-        statement_str += "\n#{transaction.date} || #{transaction.credit_amount}.00 || || #{historic_balance}.00"
+        statement_str.prepend( "\n#{transaction.date} || #{transaction.credit_amount}.00 || || #{historic_balance}.00")
       elsif transaction.debit_amount > 0
-        historic_balance += transaction.debit_amount
-        statement_str += "\n#{transaction.date} || || #{transaction.debit_amount} || #{historic_balance}.00"
+        historic_balance -= transaction.debit_amount
+        statement_str.prepend("\n#{transaction.date} || || #{transaction.debit_amount}.00 || #{historic_balance}.00")
       end
     end
+    statement_str.prepend("date || credit || debit || balance")
     statement_str
   end
 end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -4,6 +4,7 @@ describe Bank do
 
   subject(:bank) { described_class.new }
   let(:transaction) { double :transaction }
+  let(:transaction2) { double :transaction }
 
   it 'has zero balance initially' do
     expect(bank.calc_balance).to eq 0
@@ -26,5 +27,17 @@ describe Bank do
     allow(transaction).to receive(:debit_amount).and_return(0)
     bank.receive_transaction(transaction)
     expect(bank.print_statement).to eq "date || credit || debit || balance\n10/01/2012 || 1000.00 || || 1000.00"
+  end
+
+  it 'prints a statement after receiving 2 transactions' do
+    allow(transaction).to receive(:date).and_return("10/01/2012")
+    allow(transaction).to receive(:credit_amount).and_return(1000)
+    allow(transaction).to receive(:debit_amount).and_return(0)
+    allow(transaction2).to receive(:date).and_return("13/01/2012")
+    allow(transaction2).to receive(:credit_amount).and_return(0)
+    allow(transaction2).to receive(:debit_amount).and_return(500)
+    bank.receive_transaction(transaction)
+    bank.receive_transaction(transaction2)
+    expect(bank.print_statement).to eq "date || credit || debit || balance\n13/01/2012 || || 500.00 || 500.00\n10/01/2012 || 1000.00 || || 1000.00"
   end
 end
