e5e7a5353eb1def603524d437972504937363f20
Jenny Bacon <jenniferbacon01@gmail.com>
2017-10-02 11:22:17 +0100

    transaction instantiation test passing

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,2 +1,10 @@
 class Transaction
+
+  attr_reader :date, :amount
+
+  def initialize(date, amount)
+    @date = date
+    @amount = amount
+  end
+
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -1,9 +1,8 @@
 require 'transaction'
 
 describe Transaction do
-
+  subject(:transaction) { described_class.new("10-01-2012", 1000) }
   it "records the date and amount when created" do
-    subject(:transaction) { described_class.new("10-01-2012", 1000) }
     expect(transaction.date).to eq "10-01-2012"
     expect(transaction.amount).to eq 1000
   end
