524249328b6abbb25a5d8932cb309eb668cc8409
Jenny Bacon <jenniferbacon01@gmail.com>
2017-10-02 11:08:40 +0100

    failing bank receives transaction test

---
+++ spec/bank_spec.rb
@@ -0,0 +1,10 @@
+require 'bank'
+
+describe Bank do
+  subject(:bank) { described_class.new }
+  let(:transaction) { double :transaction}
+  it "creates a transaction and records it" do
+    bank.receive_transaction(transaction)
+    expect(bank.transactions[0]).to eq [transaction]
+  end
+end
