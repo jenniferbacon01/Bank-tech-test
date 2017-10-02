8e919e4553903c15b4aef1a075edc60b41454e28
Jenny Bacon <jenniferbacon01@gmail.com>
2017-10-02 11:56:14 +0100

    bank prints statement with amount passing

--- lib/bank.rb
+++ lib/bank.rb
@@ -14,7 +14,17 @@ class Bank
     @transactions.each do |transaction|
       balance += transaction.amount
     end
-    return balance
+    balance
+  end
+
+  def print_statement
+    statement_str = "date || amount || balance"
+    historic_balance = 0
+    @transactions.each do |transaction|
+      historic_balance += transaction.amount
+      statement_str += "\n#{transaction.date} || #{transaction.amount}.00 || #{historic_balance}.00"
+    end
+    statement_str
   end
 
 end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -20,11 +20,10 @@ describe Bank do
     expect(bank.calc_balance).to eq 1000
   end
 
-  # it 'prints a statement after receiving a transaction' do
-  #   allow(transaction).to receive(:date).and_return("10/01/2012")
-  #   allow(transaction).to receive(:amount).and_return(1000)
-  #   bank.receive_transaction(transaction)
-  #   expect(bank.print_statement).to eq "date || credit || debit || balance\n"
-  #   + "10/01/2012 || 1000.00 || || 1000.00"
-  # end
+  it 'prints a statement after receiving a transaction' do
+    allow(transaction).to receive(:date).and_return("10/01/2012")
+    allow(transaction).to receive(:amount).and_return(1000)
+    bank.receive_transaction(transaction)
+    expect(bank.print_statement).to eq "date || amount || balance\n10/01/2012 || 1000.00 || 1000.00"
+  end
 end
